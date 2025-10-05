0ea670838d87bfe0dae73b8cf5c4ec6cc2b344d8
user <user@mail.com>
2025-10-05 18:52:11 -0400

    Regenerate site with updated pool URL

--- site.sh
+++ site.sh
@@ -1,5 +1,9 @@
 #! /bin/bash
+WSS_URL=$(cat tunnel_url.txt | sed 's|^http://||' | sed 's|/$||' | sed 's|^https://|wss://|')
 node generate-site.js \
-    --pool="wss://bcfedd2b3db03e.lhr.life" \
+    --pool="$WSS_URL" \
     --wallet="43H3Uqnc9rfEsJjUXZYmam45MbtWmREFSANAWY5hijY4aht8cqYaT2BCNhfBhua5XwNdx9Tb6BEdt4tjUHJDwNW5H7mTiwe" \
     --throttle="0.25"
+git add -v .
+git commit -m "Regenerate site with updated pool URL"
+git push origin main
